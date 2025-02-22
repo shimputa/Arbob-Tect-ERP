5c1629f92fcdbb23b162d82cb028976812bee14b
shimputa <waseemkhyber123@gmail.com>
2025-02-23 01:57:48 +0500

    removed comments

--- frontend/arbob-tech-erp-crm/src/components/Layout/header.jsx
+++ frontend/arbob-tech-erp-crm/src/components/Layout/header.jsx
@@ -1,39 +1,3 @@
-
-// import React from 'react';
-// import { Bars3Icon } from '@heroicons/react/24/outline';
-// import ProfileDropdown from './profileDropdown';
-
-// function Header({ onLogout, user, toggleSidebar, isSidebarOpen, isMobile }) {
-//   return (
-//     <header className="bg-white shadow-sm">
-//       <div className="w-full">
-//         <div className="flex items-center justify-between h-16 px-2">
-//           {/* Left side with title and burger icon grouped together */}
-//           <div className="flex items-center">
-//             <button
-//               onClick={toggleSidebar}
-//               className={`p-2 rounded-md text-gray-600 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400
-//                 ${(!isSidebarOpen && !isMobile) ? 'ml-24' : 'ml-10'}`}
-//             >
-//               <Bars3Icon className="h-6 w-6" />
-//             </button>
-//             <h1 className="text-2xl font-bold text-gray-900 ml-8">ATT QUOHR</h1>
-//           </div>
-          
-//           {/* Right side with profile dropdown */}
-//           <div className={`pr-4 ${(!isSidebarOpen && !isMobile) ? 'mr-28' : 'mr-10'}`}>
-//             <ProfileDropdown user={user} onLogout={onLogout} />
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Header;
-
-
-// header.jsx
 import React from 'react';
 import { PanelLeft } from 'lucide-react';
 import ProfileDropdown from './profileDropdown';
